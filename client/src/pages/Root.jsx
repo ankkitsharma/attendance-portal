7062136c3a20f8636aee89fe63b23c50acbb019d
Ankit Sharma <ankitsharma2801@gmail.com>
2024-04-06 11:59:06 +0530

    add title for root

--- client/src/pages/Root.jsx
+++ client/src/pages/Root.jsx
@@ -1,10 +1,22 @@
 import React, { useContext, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import UserContext from "../context/Usercontext";
+import styles from "./Root.module.css";
 
 export default function root() {
   const { user } = useContext(UserContext);
-  useEffect(() => {
-    console.log("Inside Root: ", user);
-  }, []);
-  return <div>Hello {user?.user.username}</div>;
+  const navigate = useNavigate();
+  return (
+    <div className={styles.title}>
+      Hello{" "}
+      {user ? (
+        user.user.username
+      ) : (
+        <div>
+          You are not logged in{" "}
+          <button onClick={() => navigate("/signIn")}>Log in</button>
+        </div>
+      )}
+    </div>
+  );
 }
